f65dd2aab1acf48116c094f09ec0b1a3f7584abd
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-04-22 18:07:28 +0530

    deployment configrations

--- educase_assignment/server/server.js
+++ educase_assignment/server/server.js
@@ -1,16 +1,9 @@
 import express from 'express';
 import router from './Routes.js';
 
-const PORT = 5000
 const app = express();
 
 app.use(express.json());
 app.use('/api', router);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ msg: "Test route" })
-});
-
-app.listen(PORT, () => {
-  console.log(`server running on http://localhost:${PORT}`);
-});
+export default app;
