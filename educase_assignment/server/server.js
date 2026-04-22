916475ed424ed4390392109093f70264ef93d714
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-04-22 18:10:18 +0530

    deployment configrations

--- educase_assignment/server/server.js
+++ educase_assignment/server/server.js
@@ -5,5 +5,9 @@ const app = express();
 
 app.use(express.json());
 app.use('/api', router);
+//test route
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: "Test route" })
+})
 
 export default app;
